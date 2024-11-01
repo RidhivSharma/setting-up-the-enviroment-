00ac7697631530c414e9eb759494abcee8fa5b37
RidhivSharma <ridhiv0305@gmail.com>
2024-11-01 10:40:41 +0000

    variable and constant

---
+++ notebook/book1.js
@@ -0,0 +1,8 @@
+console.log("ridhiv")
+const accountnumber=123456
+let accountemail="vscbhnjmhgfxz"
+var acccounpassword="sdfghjk"
+accountwhat="asdfghjk"
+let test;// ; lagao ya na lagao koi farak ni padta hai 
+
+console.table([accountnumber,accountwhat,accountemail,acccounpassword,test])
