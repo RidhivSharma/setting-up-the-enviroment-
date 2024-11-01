6674a835fe87c0cd5057e8bc0578081911f9fe7e
RidhivSharma <ridhiv0305@gmail.com>
2024-11-01 11:16:54 +0000

    dataypes

---
+++ notebook/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";// what happes is in the earlier version of js some commands were differt to the new version so takle ki woh bhi na kharab ho or new version se bhi likh paye toh hamne yaha use strict kiya jiska matlab hain ki new rules ke hisab se code ko run kerke dekhe
+let accountholder="ridhiv"
+let age=18
+let male=true
+console.table([accountholder,age,male])
+console.log(typeof accountholder)
+console.log(typeof undefined)//undefined is a type 
+console.log(typeof null)// null is a object in js
